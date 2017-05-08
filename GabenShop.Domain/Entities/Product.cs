1e902634cc487c3a0a71f54f803ec6045fefb3d3
ilyak1997 <IlyaK1997@gmail.com>
2017-05-09 01:34:01 +0400

    First Comment

---
+++ GabenShop.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabenShop.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Rank { get; set; }
+
+        //Image добавить
+    }
+}
---
+++ GabenShop.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using GabenShop.Domain.Abstract;
+using GabenShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GabenShop.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
