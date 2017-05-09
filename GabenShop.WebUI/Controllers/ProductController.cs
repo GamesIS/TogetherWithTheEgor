0e340848a40f8a2fe24725f065f60aee5ecc40f3
ilyak1997 <IlyaK1997@gmail.com>
2017-05-09 13:23:11 +0400

    Странички

--- GabenShop.WebUI/Controllers/ProductController.cs
+++ GabenShop.WebUI/Controllers/ProductController.cs
@@ -5,20 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GabenShop.WebUI.Models;
 
 namespace GabenShop.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int pageSize = 4;
         public ProductController(IProductRepository repository)
         {
             this.repository = repository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            //return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(Product => Product.ProductID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ GabenShop.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GabenShop.Domain.Entities;
+
+namespace GabenShop.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
